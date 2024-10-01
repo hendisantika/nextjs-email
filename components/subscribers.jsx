14ee53425b25f31862526606f5c78f9282f14930
hendisantika <hendisantika@yahoo.co.id>
2024-10-01 08:34:24 +0700

    feat: Add subscribers.jsx 🫸🌀✏️📗🐧🐳⬆

---
+++ components/subscribers.jsx
@@ -0,0 +1,25 @@
+import {Card, CardContent,} from "@/components/ui/card";
+
+import {getSubscribers} from "@/queries";
+
+export const Subscribers = async () => {
+    const subscribers = await getSubscribers();
+    console.log(subscribers);
+    return (
+        <div className="flex flex-col justify-center items-center my-4">
+            {subscribers.map((sub) => (
+                <Card key={sub.id} className="my-2">
+                    <CardContent>
+                        <p className="my-2">Wow!! {sub.name} Subscribed.</p>
+                        <p>
+                            Subscribed on:{" "}
+                            {new Intl.DateTimeFormat("en-us").format(
+                                sub.createdAt
+                            )}
+                        </p>
+                    </CardContent>
+                </Card>
+            ))}
+        </div>
+    );
+};
